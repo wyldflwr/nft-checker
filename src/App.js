731a1ef94746f83028c67cf8d76be44b69f256b4
mrkp <mark@zed.io>
2022-05-26 15:23:19 -0400

    adding qr scanner with modal and lottie animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import {QrReader} from 'react-qr-reader';
+import {useState} from "react";
+import { CSSTransition } from 'react-transition-group';
+import Modal from 'react-modal'
+import Lottie from 'react-lottie';
+import * as animationData from './lottie/tick.json'
+const buttonStyle = {
+  display: 'block',
+  margin: '10px auto'
+};
 
-function App() {
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+}
+const Home = () => {
+  const [data, setData] = useState('No result');
+  const [isOpen, setOpen] = useState(true);
+  const [isPaused, setPaused] = useState(true);
+  const [isStopped, setStopped] = useState(true);
+  const toggleModal = () => setOpen(!isOpen);
+  const modalStyles = {
+    overlay: {
+      backgroundColor: '#000000',
+      width: '100%',
+      height: '100%'
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style={{flex: 1, backgroundColor: '#dfcccb'}}>
+        <QrReader
+
+            onResult={(result, error) => {
+              if (!!result) {
+                console.log('scanned: ', result?.text);
+                console.log('scanned');
+                setData();
+                setOpen(true)
+              }
+
+              console.log('scanned: ', result)
+            }}
+            style={{ height: '100%', width: '100%' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </QrReader>
+        <CSSTransition
+            in={isOpen}
+            timeout={300}
+            classNames="dialog"
+        >
+          <Modal
+              isOpen={isOpen}
+              style={modalStyles}
+          >
+            <Lottie options={defaultOptions}
+                    height={400}
+                    width={400}
+                    isStopped={isStopped}
+                    isPaused={isPaused}/>
+            <button onClick={toggleModal}>
+              Close Modal
+            </button>
+          </Modal>
+
+        </CSSTransition>
+        <p>{data}</p>
+      </div>
+  )
 }
 
-export default App;
+export default Home
